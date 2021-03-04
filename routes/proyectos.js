41ec217373e1f01f054711b9ee25292e36af13c7
dorian mendoza <dorian.mendoza@csiciber.com>
2021-03-04 15:56:31 -0600

    ultimo commit

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,9 +1,17 @@
 // Rutas para crear proyectos
 const router = require('express').Router()
 const proyectoController = require('../controllers/proyectoController')
+const auth = require('../middleware/auth')
+const { check } = require('express-validator')
 
 // Crear proyecto
 // /api/proyectos
-router.post('/', proyectoController.crearProyecto)
+router.post('/',    
+    auth,
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty()
+    ],
+    proyectoController.crearProyecto
+)
 
 module.exports = router
